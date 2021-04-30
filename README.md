4d489e1bffcec9b0cd4d7d83e2ecb57094508ebf
ngui27 <guilhemlaieb@gmx.fr>
2021-04-30 14:40:30 +0200

    Premier pas angular, directives structurelle.

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 
 Mon travail au sein de la formation Angular dispensée part WEBFORCE 3 en avril 2021 (formateur :[@sqyqh](https://github.com/sqyqh))
 
-##Jours 1 
+##Jours 1
+
 - Presentation d'Angular
 - Rappel HTML CSS js
 - Installation nope.js
 - Installation Angular
+- rappel Git et GitHub
+
+## Jours 2
+
+- Premiers pas Angular
+- Les directives Structurelles
+- Fin du premier PDJ
+- Commencement du Deuxieme PDJ
--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+let heroCount = 2 ;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour-of-heroes';
+  //title = '';
+  titleStyle = {'font-family' : 'Arial', 'color' : 'red'};
+  username = "Anonymous";
+  heroes = [
+    "Batman",
+    "Catwoman",
+    "Hulk",
+    "Iron man",
+    "Super man"
+  ]
+  heroToAdd= '';
+  addHero(){
+    this.heroes.push(this.heroToAdd);
+   //this.title = 'La revue des '+ this.heroes.length + 'héros';
+  }
+  
 }
