d793d85df2abd551614e5e1a4d011d7c3c623811
ngui27 <guilhemlaieb@gmx.fr>
2021-05-03 14:30:52 +0200

    Ajout de l'exercice delete

--- tour-of-heroes/src/app/app.component.ts
+++ tour-of-heroes/src/app/app.component.ts
@@ -16,11 +16,23 @@ export class AppComponent {
     "Hulk",
     "Iron man",
     "Super man"
+  ];
+  evils = [
+    'Joker',
+    'Luthor',
+    'WonderTrump',
+    'Waterman'
   ]
   heroToAdd= '';
   addHero(){
     this.heroes.push(this.heroToAdd);
    //this.title = 'La revue des '+ this.heroes.length + 'héros';
   }
+  heroToRemove = '';
+  removeHero(heroToRemove: string) {
+    this.heroes.forEach((value, index) => {
+      if (value == heroToRemove) this.heroes.splice(index, 1);
+    });
   
+}
 }
--- tour-of-heros-two/src/app/app.component.ts
+++ tour-of-heros-two/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'La tours des heros';
+  title = 'La Revue des Zhero';
 }
